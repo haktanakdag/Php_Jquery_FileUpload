944a0c146be1455f70ad86c2fa3e1834e352741b
Haktan <haktan.akdag@gmail.com>
2016-05-03 11:44:18 +0300

    Added files via upload

---
+++ head.php
@@ -0,0 +1,22 @@
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<script src="//code.jquery.com/jquery-1.10.2.js"></script>
+<script src="//code.jquery.com/ui/1.11.2/jquery-ui.js"></script>
+<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+<script src="../js/tmpl.min.js"></script>
+<script src="../js/load-image.all.min.js"></script>
+<script src="../js/jquery.blueimp-gallery.min.js"></script>
+<script src="../js/jquery.iframe-transport.js"></script>
+<script src="../js/jquery.fileupload.js"></script>
+<script src="../js/jquery.fileupload-process.js"></script>
+<script src="../js/jquery.fileupload-image.js"></script>
+<script src="../js/jquery.fileupload-audio.js"></script>
+<script src="../js/jquery.fileupload-video.js"></script>
+<script src="../js/jquery.fileupload-validate.js"></script>
+<script src="../js/jquery.fileupload-ui.js"></script>
+<script src="../js/jqueryfileuploadmain.js"></script>
+<link rel="stylesheet" href="../css/blueimp-gallery.min.css" />
+<link rel="stylesheet" href="../css/jquery.fileupload.css" type="text/css" />
+<link rel="stylesheet" href="../css/jquery.fileupload-ui.css" type="text/css" />
+</head>
---
+++ index.php
@@ -0,0 +1,3 @@
+<?php
+header("Location: ./base/dokuman.php");
+?>
